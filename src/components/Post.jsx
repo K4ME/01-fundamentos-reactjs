49faf2bc54931c34254ccc071f977e0456391141
K4ME <guilhermegodencio@gmail.com>
2022-07-12 11:59:47 -0300

    declarative programming

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,35 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { useState } from 'react'
+
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post({author, content, publishedAt}) {
+  const [comments, setComments] = useState(['Post muito bacana, hein?!']);
+  const [newCommentText, setNewCommentText] = useState('');
+
+  const publishedDateFormated = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR
+  });
+  
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  })
+
+  function handleCreateNewComment(){
+    event.preventDefault();
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
+
+  function handleNewCommentChange(){
+    setNewCommentText(event.target.value);
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -14,29 +41,37 @@ export function Post({author, content, publishedAt}) {
           </div>
         </div>
 
-        <time title='11 de maio às 08:11h' dateTime='2022-05-11 08:11:01'>Publicado há 1h</time>
+        <time 
+          title={publishedDateFormated} 
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
+        {content.map(line => {
+          if(line.type === 'paragraph'){
+            return <p>{line.content}</p>
+          }
+
+          if(line.type === 'link'){
+            return <p><a href=''>{line.content}</a></p>
+          }
+        })}
         
-        <p>Fala galeraa 👋</p>
-
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-        <p>👉{' '}<a href=''>jane.design/doctorcare</a></p>
-
-        <p>
-          <a href=''>#novoprojeto</a>{' '}
-          <a href=''>#nlw</a>{' '}
-          <a href=''>#rocketseat</a>
-        </p>
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
 
         <strong>Deixe seu feedback</strong>
 
-        <textarea placeholder='Deixe um comentário'/>
+        <textarea 
+          name='comment' 
+          placeholder='Deixe um comentário'
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+        />
 
         <footer>
           <button type='submit'>Publicar</button>
@@ -44,9 +79,10 @@ export function Post({author, content, publishedAt}) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment content={comment}/>
+        })}
+        
       </div>
     </article>
   )
